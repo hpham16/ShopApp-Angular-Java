3731bc78a916791c2fb13ca5e6c28472ca5e5820
LAPTOP-VNVT\vongo <vongovantien@gmail.com>
2024-07-11 18:21:29 +0700

    ++update env

--- ShopApp/src/app/services/coupon.service.ts
+++ ShopApp/src/app/services/coupon.service.ts
@@ -1,8 +1,7 @@
-import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from '../../environments/environment';
-import { ApiResponse } from '../responses/api.response';
 
 interface CouponResponse {
   result: number;
@@ -25,5 +24,5 @@ export class CouponService {
 
     return this.http.get<CouponResponse>(url, { params });
   }
-  
+
 }
